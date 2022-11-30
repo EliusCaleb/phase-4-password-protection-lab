1d42291c4f6ec77e18522c68e26c6f96596ddc98
EliusCaleb <eliuswanyoro@gmail.com>
2022-11-30 05:41:12 +0300

    Learning

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :authorize
+    before_action :authorize, only: [:show]
     skip_before_action :authorize, only: [:create]
    
   def create
